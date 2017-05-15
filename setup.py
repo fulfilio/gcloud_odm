8c2f5f079b058301c8fe0f586064030e50a1414d
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2017-05-15 18:52:57 +0530

    Released version 0.0.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='gcloud_odm',
-    version='0.0.1',
+    version='0.0.2',
     description="Google cloud datastore based ODM",
     long_description=readme + '\n\n' + history,
     author="Fulfil.io Inc.",
